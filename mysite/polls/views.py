852d1da80c9fbc7c55b13c0f96fa9966e6467587
Thitaseth Turongpun <bestthi2209@gmail.com>
2019-10-05 15:19:06 +0700

    add table

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -42,7 +42,7 @@ class ResultsView(generic.DetailView):
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)  
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
